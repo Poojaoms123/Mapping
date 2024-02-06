e57e209b90e7ce70275303aa7935f6c7286dd2e7
Pooja Suryawanshi <pooja.omsoftware1@gmail.com>
2024-02-06 19:14:16 +0530

    email template

--- src/main/java/com/example/company/Service/ServiceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/example/company/Service/ServiceImpl/EmployeeServiceImpl.java
@@ -1,18 +1,30 @@
 package com.example.company.Service.ServiceImpl;
 
+import com.amazonaws.util.IOUtils;
 import com.example.company.Model.Company;
 import com.example.company.Model.Employee;
 import com.example.company.Model.SaveRequest.SaveEmployeeRequest;
 import com.example.company.Repository.CompanyRepository;
 import com.example.company.Repository.EmployeeRepository;
 import com.example.company.Service.EmployeeService;
+import com.itextpdf.html2pdf.HtmlConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.*;
 import java.util.List;
 import java.util.Random;
 
@@ -26,6 +38,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     private JavaMailSender javaMailSender;
     @Autowired
     private Otpservice otpservice;
+    @Autowired
+    private TemplateEngine templateEngine;
+
 
     @Override
     public Object saveOrUpdateEmployee(SaveEmployeeRequest saveEmployeeRequest, Long companyId) throws Exception {
@@ -70,6 +85,17 @@ public class EmployeeServiceImpl implements EmployeeService {
             int otp = otpservice.generateOtp(saveEmployeeRequest.getEmployeeEmail());
             int serverOtp = otpservice.getOtp(saveEmployeeRequest.getEmployeeEmail());
             otpservice.clearOTP(saveEmployeeRequest.getEmployeeEmail());
+            String htmlConent = "<html><body><h1>Hi This Is Html Template</h1></body></html>";
+            Context context=new Context();
+            context.setVariable("name",employee.getEmployeeName());
+            String process = templateEngine.process("emai-template.html", context);
+            File file = File.createTempFile("template",".pdf");
+            InputStream inputStream=new FileInputStream(file);
+            FileInputStream fileOutputStream=new FileInputStream(file);
+            MultipartFile multipartFile=new MockMultipartFile("converted",file.getName(),".pdf", IOUtils.toByteArray(fileOutputStream));
+            HtmlConverter.convertToPdf(process,new FileOutputStream(file));
+            this.sendEmailWithAttachment("suryawanshipooja.sp@gmail.com","Application","hello",multipartFile);
+            this.sendEmailWithHtmlTemplate("suryawanshipooja.sp@gmail.com","Application",process);
             this.sendEmail("suryawanshipooja.sp@gmail.com", "Application", "Hi pooja your otp is " + otp);
 
             return "save sucessfully";
@@ -91,6 +117,37 @@ public class EmployeeServiceImpl implements EmployeeService {
         mailMessage.setText(body);
         javaMailSender.send(mailMessage);
 
+    }
+    public void sendEmailWithHtmlTemplate(String toEmail, String subject, String body) {
+
+        MimeMessage mimeMessage =javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+        try {
+            helper.setTo(toEmail);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+           e.printStackTrace();
+        }
+    }
+
+    public void sendEmailWithAttachment(String toEmail, String subject, String body, MultipartFile attachment) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setTo(toEmail);
+            helper.setSubject(subject);
+            helper.setText(body);
+
+      //      FileSystemResource file = new FileSystemResource(new File(attachment));
+            helper.addAttachment(attachment.getOriginalFilename(),new ByteArrayResource(attachment.getBytes()));
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException | IOException e) {
+           e.printStackTrace();
+        }
     }
 
 
